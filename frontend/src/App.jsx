b917cb1b59134ca1175f62c59d351e578d20856d
Sepang2 <sepang_2@naver.com>
2023-12-07 16:57:28 +0900

    make page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Main from "./pages/main";
+import Mint from "./pages/mint";
+
 const App = () => {
   return (
-    <div className="bg-red-100 min-h-screen flex justify-center items-center">
-      App
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/mint" element={<Mint />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
---
+++ frontend/src/pages/mint.jsx
@@ -0,0 +1,9 @@
+const Mint = () => {
+  return (
+    <div className="bg-red-100 min-h-screen flex flex-col justify-center items-center">
+      <button className="bg-blue-100">Mint</button>
+    </div>
+  );
+};
+
+export default Mint;

b5e4b00a4b60173c76fdd7fac94350176a9fb330
Sepang2 <sepang_2@naver.com>
2023-12-07 16:31:14 +0900

    first commit

---
+++ contracts/MintNFT.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+
+contract MintNFT is ERC721Enumerable {
+    string metadataURI;
+    uint maxSupply;
+
+    constructor(string memory _name, string memory _symbol, string memory _metadataURI, uint _maxSupply) ERC721(_name, _symbol) {
+        metadataURI = _metadataURI;
+        maxSupply = _maxSupply;
+    }
+
+    function mintNFT() public {
+        require(totalSupply() < maxSupply, "No more mint.");
+
+        uint tokenId = totalSupply() + 1;
+
+        _mint(msg.sender, tokenId);
+    }
+
+    function batchMint(uint _amount) public {
+        for(uint i = 0; i < _amount; i++){
+            mintNFT();
+        }
+    }
+
+    function tokenURI(uint _tokenId) public view override returns(string memory){
+        return string(abi.encodePacked(metadataURI, '/', Strings.toString(_tokenId), '.json'));
+    }
+}

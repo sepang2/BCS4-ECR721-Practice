dda7ea24383986d3851f0aeaed83e71ec0a83986
Sepang2 <sepang_2@naver.com>
2023-12-08 15:00:04 +0900

    Update for useContext

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,56 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./pages/main";
 import Mint from "./pages/mint";
+import Header from "./components/Header";
+import { createContext, useEffect, useState } from "react";
+import { useSDK } from "@metamask/sdk-react";
+import Web3 from "web3";
+import mintNFTABI from "./mintNFT.json";
+
+export const AppContext = createContext({
+  account: "",
+  setAccountHandler: () => {},
+  web3: undefined,
+  contract: undefined,
+});
 
 const App = () => {
+  const [account, setAccount] = useState("");
+  const setAccountHandler = (state) => setAccount(state);
+
+  const [web3, setWeb3] = useState();
+  const [contract, setContract] = useState();
+
+  const { provider } = useSDK();
+
+  useEffect(() => {
+    if (!provider) return;
+
+    setWeb3(new Web3(provider));
+  }, [provider]);
+
+  useEffect(() => {
+    if (!web3) return;
+
+    setContract(
+      new web3.eth.Contract(
+        mintNFTABI,
+        "0xab52133914cafedef17344fd784586f792a8bde2"
+      )
+    );
+  }, [web3]);
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/mint" element={<Mint />} />
-      </Routes>
-    </BrowserRouter>
+    <AppContext.Provider value={{ account, setAccountHandler, web3, contract }}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Header />}>
+            <Route path="/" element={<Main />} />
+            <Route path="/mint" element={<Mint />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </AppContext.Provider>
   );
 };
 
--- frontend/src/pages/mint.jsx
+++ frontend/src/pages/mint.jsx
@@ -1,33 +1,11 @@
-import { useSDK } from "@metamask/sdk-react";
-import { useEffect, useState } from "react";
-import Web3 from "web3";
-import mintNFTABI from "../mintNFT.json";
+import { useContext, useState } from "react";
 import axios from "axios";
+import { AppContext } from "../App";
 
 const Mint = () => {
-  const { sdk, provider } = useSDK();
-  const [web3, setWeb3] = useState();
-  const [account, setAccount] = useState();
-  const [contract, setContract] = useState();
+  const { contract, account } = useContext(AppContext);
+
   const [metadata, setMetadata] = useState();
-
-  const onClickMetaMask = async () => {
-    try {
-      const accounts = await sdk?.connect();
-
-      setAccount(accounts[0]);
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
-  const onClickLogout = async () => {
-    try {
-      setAccount("");
-    } catch (err) {
-      console.error(err);
-    }
-  };
 
   const onClickMint = async () => {
     try {
@@ -54,58 +32,15 @@ const Mint = () => {
     }
   };
 
-  useEffect(() => {
-    if (!provider) return;
-
-    setWeb3(new Web3(provider));
-  }, [provider]);
-
-  useEffect(() => {
-    if (!web3) return;
-
-    setContract(
-      new web3.eth.Contract(
-        mintNFTABI,
-        "0xab52133914cafedef17344fd784586f792a8bde2"
-      )
-    );
-  }, [web3]);
-
   return (
     <div className="bg-red-100 min-h-screen max-w-screen-md mx-auto flex flex-col justify-center items-center">
-      <div className="bg-blue-100 w-full fixed top-0 max-w-screen-md">
-        {account ? (
-          <>
-            <div>
-              {account.substring(0, 7)}...
-              {account.substring(account.length - 5)}
-            </div>
-
-            <button
-              onClick={onClickMint}
-              className="bg-cyan-100 font-bold text-xl rounded-full px-4 py-1"
-            >
-              🌱 Mint
-            </button>
-            <button
-              onClick={onClickLogout}
-              className="border border-black text-xl font-semibold rounded-full px-2 py-1"
-            >
-              🔒
-            </button>
-            <div>
-              {metadata && <img src={metadata.image} alt={metadata.name} />}
-            </div>
-          </>
-        ) : (
-          <button
-            onClick={onClickMetaMask}
-            className="rounded-full px-4 py-1 text-xl font-semibold bg-amber-300 hover:bg-amber-500 focus:bg-amber-300 hover:animate-bounce"
-          >
-            🦊 Login
-          </button>
-        )}
-      </div>
+      <button
+        onClick={onClickMint}
+        className="bg-cyan-100 font-bold text-xl rounded-full px-4 py-1 hover:bg-cyan-300 focus:bg-cyan-100"
+      >
+        🌱 Mint
+      </button>
+      <div>{metadata && <img src={metadata.image} alt={metadata.name} />}</div>
     </div>
   );
 };

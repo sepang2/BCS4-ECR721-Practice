b005993b7d593ca29c7622910dc59e17001bd22b
Sepang2 <sepang_2@naver.com>
2023-12-11 10:31:33 +0900

    Update .gitignore

--- frontend/src/pages/main.jsx
+++ frontend/src/pages/main.jsx
@@ -1,5 +1,9 @@
 const Main = () => {
-  return;
+  return (
+    <div className="bg-green-100 min-h-screen max-w-screen-md mx-auto flex flex-col justify-center items-center">
+      Main
+    </div>
+  );
 };
 
 export default Main;
